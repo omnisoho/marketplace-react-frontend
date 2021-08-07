edf22fcd329c5608919ae73a40fbd32cd7a39c08
Glen <glen@Glens-MacBook-Pro.local>
2021-08-07 23:37:23 +0800

    -create login component to let user login with username & password -create MyProducts component to show own list of products

---
+++ src/components/MyProducts.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+
+const SERVER_URL = 'http://localhost:8080/';
+
+const MyProductsList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    retrieveProducts();
+  }, []);
+
+  const retrieveProducts = () => {};
+
+  return (
+    <div className="col-9">
+      <div className="row mb-3">
+        <div className="h1 col-6">My Products</div>
+        <div className="col-6"></div>
+      </div>
+      {products &&
+        products.map((product, index) => (
+          <div key={index} className="card mb-3 p-3">
+            <div className="row no-gutters">
+              <div className="col-3">
+                <img
+                  src={SERVER_URL + product.imageUrl}
+                  className="card-img"
+                  alt=".."
+                  style={{ width: 200 }}
+                />
+              </div>
+              <div className="col-9">
+                <div className="card-body">
+                  <h5 className="card-title">{product.name}</h5>
+                  <h4 className="card-title">{product.price}</h4>
+                  <p className="card-text">{product.description}</p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default MyProductsList;
